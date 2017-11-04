f3675b496aced9415dd0b8770ba1f7ef1314cd47
James Drawneek <jmdrawneek@googlemail.com>
2017-11-04 22:42:39 +0000

    Pass console logs to parent process.

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ const webserver = webServer(config.runCommand, config.commandArgs);
 
 function webServer (cmd, commandArgs) {
   console.log(path.join(__dirname, '..'));
-  const webserver = spawnSync(`npx`, [cmd, commandArgs], {cwd: path.join(__dirname, '..')});
+  const webserver = spawnSync(`npx`, [cmd, commandArgs], {
+    cwd: path.join(__dirname, '..'),
+    stdio: [0,1,2]
+  });
 
   webserver.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
@@ -50,7 +53,9 @@ function webServer (cmd, commandArgs) {
 
 
 function pingUrl (url) {
-  const pingProcess = spawnSync(`ping`, [`-c 30`, `${url}`]);
+  const pingProcess = spawnSync(`ping`, [`-c 30`, `${url}`], {
+    stdio:[0,1,2]
+  });
 
   pingProcess.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
