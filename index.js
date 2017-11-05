72846804a3bb4f6c208d238d6a3dc3dab828042c
James Drawneek <jmdrawneek@googlemail.com>
2017-11-05 00:10:29 +0000

    no message

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ webserver.on('close', (code) => {
 function pingUrl (url, webserver) {
   const pingProcess = spawnSync(`ping`, [`-c 30`, `${url}`]);
   console.log('SENT PING');
-  console.log(pingProcess.stdout);
+  console.log(pingProcess.stdout.toString());
 
   webserver.kill('SIGHUP');
 
